b4852f4af0dc46a927d23fffbdd343a0710e2321
connorwarme <connor.warme@gmail.com>
2022-09-22 16:03:39 -0600

    Add level order traverse

--- bst.js
+++ bst.js
@@ -26,7 +26,6 @@ const removeDuplicates = (array) => {
 const buildTree = (array, start, end) => {
     if (start > end) return null;
     const mid = parseInt((start + end)/2);
-    console.log(mid);
     const node = NodeFactory(array[mid]);
     node.left = buildTree(array, start, mid-1);
     node.right = buildTree(array, mid+1, end);
@@ -64,10 +63,14 @@ const search = (root, value) => {
         return search(root.left, value);
     }
 }
+// console.log(search(bst.root, 67));
+
 
 // check if value already exists in array
 // true means it can be added to array
 // false means it's already present in array
+// problem here is that the insert fn doesn't add the value to the main array !!!
+// so do I need to use the search instead? !!!
 const checkValue = (value) => {
     let check = mainArray.find(index => {
         return index == value;
@@ -101,5 +104,76 @@ const insertRec = (root, value) => {
     }
     return root;
 }
-insert(bst.root, 2);
-prettyPrint(bst.root);
+// insert(bst.root, 2);
+// prettyPrint(bst.root);
+
+// minimum value
+const min = (root) => {
+    let minValue = root.data;
+    while (root.left != null) {
+        minValue = root.left.data;
+        root = root.left;
+    }
+    return minValue;
+}
+// delete node
+// 3 cases:
+// 1) node is a leaf
+// 2) node has one child
+// 3) node has two children
+const deletion = (root, value) => {
+    if (root == null) {
+        return root;
+    }
+    if (root.data < value) {
+        root.right = deletion(root.right, value);
+    }
+    else if (root.data > value) {
+        root.left = deletion(root.left, value);
+    }
+    else {
+        if (root.left == root.right) {
+            return null;
+        }
+        else if (root.left == null) {
+            return root.right;
+        }
+        else if (root.right == null) {
+            return root.left;
+        }
+        root.data = min(root.right);
+        root.right = deletion(root.right, root.data);
+    }
+    prettyPrint(root);
+    return root;
+}
+// deletion(bst.root, 67);
+
+// 
+function levelOrder(root, funct = 'none') {
+    if (root == null) return null;
+    let queue = [root];
+    let array = [];
+    while (queue.length != 0) {
+        let pop = queue.shift();
+        if (pop.left != null) {
+            queue.push(pop.left);
+        }
+        if (pop.right != null) {
+            queue.push(pop.right);
+            }
+        if (funct != 'none') {
+            funct(pop);
+        } else {
+            array.push(pop);
+        }  
+    }
+    if (funct == 'none') {
+        return array;
+    }
+}
+const cb = (input) => {
+    console.log(input.data);
+}
+levelOrder(bst.root, cb);
+console.log(levelOrder(bst.root));
